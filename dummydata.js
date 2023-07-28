78c8660b919b353848ddc0d8b79fe32557426fc8
Malek Ounis <133593841+Iline123@users.noreply.github.com>
2023-07-28 11:42:06 +0200

    Add files via upload

---
+++ dummydata.js
@@ -0,0 +1,96 @@
+export const homeAbout = [
+    {
+        id: 1,
+        cover: require('./images/1.png'),
+        title: "Brand Introduction",
+        desc: "Animeverse is a leading online store specializing in providing a wide range of high-quality merchandise. At Animeverse, we strive to create an immersive shopping experience where you can find unique and authentic items that celebrate your favorite characters and series.",
+    },
+    {
+        id: 2,
+        cover: require('./images/3.png'),   
+             title: "Expertise and Quality",
+        desc: "We're proud of our expertise and commitment to providing animation products of exceptional quality. From meticulously crafted figurines to high-end apparel and accessories, we prioritize the highest quality materials and attention to detail.",
+    },
+    {
+        id: 3,
+        cover: require('./images/2.png'),   
+        title: "Customer Satisfaction",
+        desc: "we are committed to ensuring your ultimate satisfaction as a customer. We believe that your experience with us goes beyond the products you buy. That's why we strive to provide you with exceptional customer service and to support you every step of the way. So we are committed to creating a positive and rewarding shopping experience for all our customers.  ",
+    },
+    
+]
+export const categoriesCard = [
+    {
+      id: 4,
+      cover: require('./images/4.png'),
+      title: "Home Decor & Collectibles"
+    },
+    {
+      id: 5,
+      cover: require('./images/6.png'),
+      title: "Apparel and Accessories"
+    },
+    {
+      id: 6,
+      cover: require('./images/5.png'),
+      title: "Figures and Collectibles"
+    }
+  ];
+  export const shop = [
+    {
+      id: 5,
+      cover: require('./images/7.jpg'),
+      title: "Kilua Led 3D Hunter x Hunter"
+    },
+    {
+      id: 6,
+      cover: require('./images/8.jpg'),
+      title: "Yuji Itadori Night lamp"
+    },
+    
+    {
+      id: 8,
+      cover: require('./images/10.jpg'),
+      title: "Jujutsu Kaisen Carpet"
+    },
+    {
+      id: 9,
+      cover: require('./images/11.jpg'),
+      title: "Demon Slayer Carpet"
+    },
+    {
+      id: 10,
+      cover: require('./images/12.jpg'),
+      title: "Tokyo Ghoul Wall Art"
+    },
+    {
+      id: 11,
+      cover: require('./images/13.jpg'),
+      title: "Naruto Wall Art"
+    }, {
+      id: 12,
+      cover: require('./images/14.jpg'),
+      title: "Dazai Ossamu Pillow"
+    },
+    {
+      id: 13,
+      cover: require('./images/15.jpg'),
+      title: "Gojo Sataru Pillow"
+    },
+    {
+      id: 14,
+      cover: require('./images/16.jpg'),
+      title: "Lufy  Coffee Mug Tea "
+    },
+    {
+      id: 15,
+      cover: require('./images/17.jpg'),
+      title: "Naruto Thermos Cup"
+    },{
+      id: 16,
+      cover: require('./images/18.jpg'),
+      title: "Hinata Car Tumbler Cup Custom "
+    },
+
+  ]
+  
