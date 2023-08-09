1f8f33b624dcc02a7dc5cfbefe5c70062917dd0e
unknown <hagartalbi@gmail.com>
2023-08-09 13:50:08 +0100

    Add Account menu

---
+++ Client/src/components/global/AccountMenu/AccountMenu.jsx
@@ -0,0 +1,105 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Tooltip from '@mui/material/Tooltip';
+import PersonAdd from '@mui/icons-material/PersonAdd';
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../Redux/userReducer'
+import { useSelector } from "react-redux";
+import {  useTheme } from "@mui/material/styles";
+
+export default function AccountMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    dispatch(logout()); // Dispatch the logout action
+    handleClose(); // Close the menu after logout
+  };
+  const user=useSelector((state) => state.user.currentUser);
+  const theme = useTheme();
+
+
+ 
+  return (
+    <React.Fragment>
+      <div theme={theme}>
+      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+        <Tooltip title="Account settings">
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{ ml: 2 }}
+            aria-controls={open ? 'account-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+          >
+            <Avatar sx={{ width: 32, height: 32 }}>{user.firstName[0]}</Avatar>
+          </IconButton>
+        </Tooltip>
+      </Box>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+            
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+        
+      >
+        
+       
+        <MenuItem  onClick={() => { handleClose(); handleLogout(); }}   sx={{
+            color: theme.palette.primary.main            }}>
+          <ListItemIcon >
+            <Logout  sx={{fill:theme.palette.primary.main}}fontSize="small"  />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+      </div>
+    </React.Fragment>
+  );
+}
