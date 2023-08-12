fa4824d865a01d42e9d6810dd7ba657b6c045567
ArfaouiBadi <ArfaouiBadi19@gmail.com>
2023-08-12 23:27:17 +0100

    last touches

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(cors({
 app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "https://animeverse-front.vercel.app");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE,PATCH");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
