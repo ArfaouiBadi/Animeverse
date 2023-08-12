6efa06b9a24e8651eba5bbe8526a2c470d4eeada
ArfaouiBadi <ArfaouiBadi19@gmail.com>
2023-08-12 17:38:58 +0100

    fixing cors

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(cors({
 // Add this middleware to set the Access-Control-Allow-Origin header
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://animeverse-front.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE","PATCH");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
