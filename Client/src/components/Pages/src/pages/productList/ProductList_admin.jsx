65405427a01983cb20fbff51e74348fee915c32d
ArfaouiBadi <ArfaouiBadi19@gmail.com>
2023-08-12 23:30:23 +0100

    last touches

--- Client/src/components/Pages/src/pages/productList/ProductList_admin.jsx
+++ Client/src/components/Pages/src/pages/productList/ProductList_admin.jsx
@@ -72,6 +72,7 @@ export default function ProductList() {
     <div className="productList">
       <DataGrid
         rows={products}
+        style={{color:"black"}}
         disableSelectionOnClick
         columns={columns}
         getRowId={(row) => row._id}
