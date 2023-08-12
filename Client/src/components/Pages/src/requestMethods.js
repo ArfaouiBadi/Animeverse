603590a270eaaf09d85b5c79dc284771bba50c87
ArfaouiBadi <ArfaouiBadi19@gmail.com>
2023-08-12 17:02:49 +0100

    fixing Token and Deploying

--- Client/src/components/Pages/src/requestMethods.js
+++ Client/src/components/Pages/src/requestMethods.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 const BASE_URL = "https://animeverse-one.vercel.app/";
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)?.currentUser?.accessToken;
-console.log(TOKEN);
+
+
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
 });
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  headers: { token: `Bearer ${TOKEN}` },
+  
 });
