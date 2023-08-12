9c835c220fcbabf2de797fa5bc8aab69f3e11cd9
ArfaouiBadi <ArfaouiBadi19@gmail.com>
2023-08-12 16:50:21 +0100

    last changes

--- Client/src/components/global/Catagories/Catagories.jsx
+++ Client/src/components/global/Catagories/Catagories.jsx
@@ -1,6 +1,6 @@
 
 import styled from "styled-components";
-import './catagories.css'
+import './Catagories.css'
 import { mobile } from "../../responsive";
 import { Link } from "react-router-dom";
 import Filter from "../Filter/Filter";
