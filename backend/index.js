99432eff196421ce3ab031801625022fbd798641
ArfaouiBadi <ArfaouiBadi19@gmail.com>
2023-08-12 18:24:32 +0100

    deploying backend

--- backend/index.js
+++ backend/index.js
@@ -6,44 +6,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
-app.use(cors());
-
 app.use(express.json());
-app.use((req, res, next) => {
-  console.log(req.path, req.method);
-  next();
-});
-
-const stripe = require("stripe")(process.env.STRIPE);
-
-app.post("/check", async (req, res) => {
-  try {
-    console.log(req.body.cart.products);
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
-      mode: "payment",
-      line_items: await req.body.cart.products.map((item) => {
-        return {
-          price_data: {
-            currency: "usd",
-            product_data: {
-              name: item.title,
-            },
-            unit_amount: item.price * 100,
-          },
-          quantity: item.quantity,
-        };
-      }),
-      success_url: `${process.env.SERVER}success`,
-      cancel_url: `${process.env.SERVER}cancel`,
-    });
-    console.log(session.url);
-    res.status(200).json({ url: session.url });
-  } catch (err) {
-    res.status(500).json({ err: err.message });
-  }
-});
+app.use(cors());
 
 // Routes
 app.use("/user", userRoutes);
@@ -54,7 +18,7 @@ app.use("/api/orders", ordersRoutes);
 mongoose.connect(process.env.URL).then(() => {
   console.log("🟢 Connected To DataBase");
   // Listen for requests
-  const PORT = process.env.PORT || 3002;
+  const PORT = 3002;
   app.listen(PORT, () => {
     console.log(`🟢 Server started on port ${PORT}`);
   });
