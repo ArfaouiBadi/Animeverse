cbf4f4d28013377171068f42bd32ede735c675eb
ArfaouiBadi <ArfaouiBadi19@gmail.com>
2023-08-12 17:30:54 +0100

    fixing cors credentienls

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,11 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(cors(
-  
+  {
+    origin:["https://animeverse-front.vercel.app/"],
+    methods:["POST","GET","DELETE","PATCH","PUT"],
+    credentials: true,
+  }
 ));
 
 app.use(express.json());
